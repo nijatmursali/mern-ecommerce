197b51ff552fe8347939817e0c0ffe6c3cb6945e
NIJAT MURSALI <nmursali2019@ada.edu.az>
2020-10-12 22:10:09 +0200

    some movements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
     <div>
-
+      <Header/>
+      <main>
+        <h1>Welcome to our shop!</h1>
+      </main>
+      <Footer/>
     </div>
   );
 }
