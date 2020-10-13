c192b12ac0289c9fc5ac39d342c2d9d132b40fb2
NIJAT MURSALI <nmursali2019@ada.edu.az>
2020-10-13 22:03:12 +0200

    setting environments

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const products = require("./data/products");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API is running");
+  res.send("API is running!!");
 });
 
 app.get("/api/products", (req, res) => {
@@ -15,4 +19,12 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running at port 5000"));
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
+
+app.listen(PORT, console.log(`Server running in ${NODE_ENV} at port ${PORT}`));
+
+process.on("SIGINT", () => {
+  console.log("Bye bye!");
+  process.exit();
+});
